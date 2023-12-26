3cafae2e9ad0fb2f649de285079678d97b1395eb
yannis <yannisdewolf@gmail.com>
2023-12-26 09:52:33 +0100

    refresh properties during runtime

--- src/main/java/be/dewolf/recipes/recipeservice/config/ApplicationProperties.java
+++ src/main/java/be/dewolf/recipes/recipeservice/config/ApplicationProperties.java
@@ -2,6 +2,7 @@ package be.dewolf.recipes.recipeservice.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @Data
 @ConfigurationProperties(prefix = "app.recipe")
